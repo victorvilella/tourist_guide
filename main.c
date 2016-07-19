2b199d2d922716d34ee10b97c0fa23ad87767f32
Victor Vilella <victor.cbl@gmail.com>
2016-07-19 12:34:23 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: victor
+ *
+ * Created on 19 de Julho de 2016, 12:21
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ * Declaração de funções
+ */
+int main(int arc, char** argv);
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    return (EXIT_SUCCESS);
+}
+
