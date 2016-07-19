91da15a044e07107203bb02b6a8eb1361519c622
Victor Vilella <victor.cbl@gmail.com>
2016-07-19 13:09:36 -0300

    Algumas coisinhas

--- main.c
+++ main.c
@@ -13,18 +13,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "graph.h"
 
 /*
  * Declaração de funções
  */
 int main(int arc, char** argv);
-
-
+void menu_principal();
+void ler_entradas(int * a, int * b);
+    
 /*
  * 
  */
 int main(int argc, char** argv) {
+    int num_cidades, num_arestas;
+    
+    menu_principal();
+    ler_entradas(&num_cidades, &num_arestas);
+    printf("%d - %d", num_cidades, num_arestas);
+    
     return (EXIT_SUCCESS);
 }
 
+void menu_principal(){
+    printf("Aqui entra um menuzinho em ASCII\n");
+    return;
+}
+
+void ler_entradas(int * a, int * b){
+    /**
+     * Critica do trabalho (Cidades não podem ultrapassar 100 unidades
+     */
+    do {
+        printf("Ler o numero de cidades: ");
+        scanf("%d", a);
+    } while(*a > 100);
+    
+    printf("Ler o numero de estradas: ");
+    scanf("%d", b);
+    return;
+}
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/graph.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tourist_guide: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tourist_guide ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/graph.o: graph.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/graph.o graph.c
 
 ${OBJECTDIR}/main.o: main.c 
 	${MKDIR} -p ${OBJECTDIR}
